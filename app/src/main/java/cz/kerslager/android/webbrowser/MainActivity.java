20ec344679288077835e939ce6c80711307887f6
Milan Kerslager <milan.kerslager@pslib.cz>
2017-04-28 10:20:03 +0200

    check URL more deeply cosmetic changes

--- app/src/main/java/cz/kerslager/android/webbrowser/MainActivity.java
+++ app/src/main/java/cz/kerslager/android/webbrowser/MainActivity.java
@@ -1,21 +1,25 @@
 package cz.kerslager.android.webbrowser;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
+import android.view.inputmethod.InputMethodManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
     WebView webView;
     EditText editTextURL;
+    final static String GOOGLE = "https://www.google.cz";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,14 +45,18 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setBuiltInZoomControls(true);
         webView.getSettings().setDisplayZoomControls(false);
         // nahrajeme první stránku
-        webView.loadUrl("https://www.google.cz/");
+        webView.loadUrl(GOOGLE);
+        editTextURL.setText(GOOGLE);
     }
 
     // tlačítko zpět bude sloužit k návratu na předchozí stránku
     @Override
     public void onBackPressed() {
         if (webView.canGoBack()) {
+            Toast.makeText(this, "Jdu na předchozí stránku", Toast.LENGTH_SHORT).show();
             webView.goBack();
+        } else {
+            Toast.makeText(this, "Už jste na první stránce", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -65,11 +73,22 @@ public class MainActivity extends AppCompatActivity {
         final URL url;
         try {
             url = new URL(vstup);
-        } catch (Exception e1) {
-            Toast.makeText(this, "Chyba v odkazu: " + vstup, Toast.LENGTH_LONG).show();
+            url.toURI();
+        } catch (MalformedURLException e) {
+            Toast.makeText(this, "Chyba v odkazu " + vstup, Toast.LENGTH_LONG).show();
+        } catch (URISyntaxException e) {
+            Toast.makeText(this, "Chyba v odkazu " + vstup, Toast.LENGTH_LONG).show();
         }
-        Toast.makeText(this, "Otevírám: " + vstup, Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Otevírám " + vstup, Toast.LENGTH_LONG).show();
+        // zkusíme nahrát novou webovou stránku
         webView.loadUrl(vstup);
+        // zhasneme softwarovou klávesnici
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+        // odstraníme kurzor ze vstupního políčka
         editTextURL.clearFocus();
     }
 }
